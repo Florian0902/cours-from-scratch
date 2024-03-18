c809d8f290398325de94661c214ecbd18006987b
Florian0902 <florian.bocquet.pro@gmail.com>
2024-03-18 16:20:15 +0100

    javascript

---
+++ JavaScript/React/redux/crypto-app/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+
+//redux
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true
+})
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
